cf83930f1232fee5dfde1f46a92014ddf4796b80
Vinay Krishnamurthy <vinayk.url@gmail.com>
2013-05-13 00:54:21 -0400

    Added auto-completion of angle brackets]

--- emacs_init.el
+++ emacs_init.el
@@ -4,6 +4,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,7 +45,6 @@
      '("\\.\\(CC?\\|cc\\|cxx\\|cpp\\|c++\\)\\'" . "C++ skeleton")
      '( \n
        "#include <iostream>" \n \n
-       "#define null NULL" \n \n
        "using namespace std;" \n \n
        "int main()" \n 
        "{" \n
@@ -57,7 +58,6 @@
      '("\\.c\\'" . "C skeleton")
      '( \n
        "#include <stdio.h>" \n \n
-       "#define null NULL" \n \n
        "int main()" \n
        "{" \n
        > _ \n
@@ -150,6 +150,18 @@
 
 (eval-after-load 'cc-mode
   '(define-key c-mode-base-map "[" 'my-c-mode-insert-lsquare))
+
+;; Insert closing square brackets in C/C++ files
+(defun my-c-mode-insert-langle ()
+  (interactive)
+  (insert "<")
+  (let ((pps (syntax-ppss)))
+    (when (and (eolp) (not (or (nth 3 pps) (nth 4 pps)))) ;; EOL and not in string or comment
+      (insert ">")
+      (forward-char -1))))
+
+(eval-after-load 'cc-mode
+  '(define-key c-mode-base-map "<" 'my-c-mode-insert-langle))
 
 
 ;; Keyboard shortcuts
